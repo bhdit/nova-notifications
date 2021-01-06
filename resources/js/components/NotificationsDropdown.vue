d7e0b79cac7fdcbed0738813d42c4a9cea25f025
Tudor Popescu <tudor.popescu@bhdit.com>
2021-01-06 20:22:07 +0200

    handle actions on toast

--- resources/js/components/NotificationsDropdown.vue
+++ resources/js/components/NotificationsDropdown.vue
@@ -1,6 +1,6 @@
 <template>
-    <dropdown class="ml-auto h-9 flex items-center dropdown-right">
-        <dropdown-trigger class="h-9 flex items-center">
+  <dropdown class="ml-auto h-9 flex items-center dropdown-right">
+    <dropdown-trigger class="h-9 flex items-center">
         <span class="relative">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ml-2 text-90 bg-gray-800 w-8">
                 <path
@@ -14,139 +14,153 @@
                 <span v-else>9+</span>
             </span>
         </span>
-        </dropdown-trigger>
+    </dropdown-trigger>
 
-        <dropdown-menu slot="menu" width="600" direction="rtl">
-            <loading-view :loading="isLoading">
-                <div class="flex justify-between bg-40 text-90 p-4">
-                    <h3>{{ __('Notifications') }}</h3>
+    <dropdown-menu slot="menu" width="600" direction="rtl">
+      <loading-view :loading="isLoading">
+        <div class="flex justify-between bg-40 text-90 p-4">
+          <h3>{{ __('Notifications') }}</h3>
 
-                    <button v-if="count !== 0" class="btn" @click="markAllAsRead()">
-                        {{ __('mark all as read') }}
-                    </button>
-                </div>
-                <p v-if="count === 0" class="block p-3">
-                    {{ __('No new notifications') }}
-                </p>
-                <scroll-wrap v-else height="350">
-                    <slot>
-                        <notification-item
-                            :ref="'notification-' + notification.id"
-                            v-for="notification in notifications"
-                            :key="notification.id"
-                            :notification="notification"
-                        >
-                        </notification-item>
-                    </slot>
-                </scroll-wrap>
-            </loading-view>
-        </dropdown-menu>
-    </dropdown>
+          <button v-if="count !== 0" class="btn" @click="markAllAsRead()">
+            {{ __('mark all as read') }}
+          </button>
+        </div>
+        <p v-if="count === 0" class="block p-3">
+          {{ __('No new notifications') }}
+        </p>
+        <scroll-wrap v-else height="350">
+          <slot>
+            <notification-item
+                :ref="'notification-' + notification.id"
+                v-for="notification in notifications"
+                :key="notification.id"
+                :notification="notification"
+            >
+            </notification-item>
+          </slot>
+        </scroll-wrap>
+      </loading-view>
+    </dropdown-menu>
+  </dropdown>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                count: 0,
-                notifications: [],
-                isLoading: true,
-            }
-        },
-        mounted() {
-            const self = this
-
-            self.loadNotifications(function (response) {
-                Echo.private(Nova.config.user_model_namespace + '.' + response.data.user_id)
-                    .notification(self.notificationReceived)
-            })
-
-            Nova.$on('notification-read', function (e) {
-                self.notifications[e.notification.id] = e.notification
-                self.count--;
-            })
-        },
-        methods: {
-            loadNotifications: function (callback) {
-                axios
-                    .get('/nova-vendor/nova-notifications/unread')
-                    .then(response => {
-                        this.isLoading = false
-                        this.count = response.data.count
-                        this.notifications = response.data.notifications
-
-                        if (callback) {
-                            callback(response)
-                        }
-                    })
-            },
-            notificationReceived: function (notification) {
-                const self = this
-
-                self.loadNotifications()
-
-                let level = 'info'
-                const levels = ['success', 'info', 'error']
-
-                if (levels.indexOf(notification.level) !== -1) {
-                    level = notification.level
-                }
-
-                const markAsRead = {
-                    text: self.__('Mark as Read'),
-                    onClick: (e, toast) => {
-                        self.$refs['notification-' + notification.id][0].markAsRead()
-                        toast.goAway(0);
-                    }
-                }
-
-                const cancel = {
-                    text: self.__('Cancel'),
-                    onClick: (e, toast) => {
-                        toast.goAway(0);
-                    }
-                }
-
-                let actions = notification.actions;
-
-                if (notification.show_mark_as_read) {
-                    actions.push(markAsRead)
-                }
-
-                if (notification.show_cancel) {
-                    actions.push(cancel)
-                }
-
-                self.$toasted.show(notification.title, {
-                    type: level,
-                    keepOnHover: true,
-                    icon: notification.icon || null,
-                    iconPack: 'custom-class',
-                    action: actions,
-                })
-            },
-            markAllAsRead: function () {
-                axios
-                    .patch('/nova-vendor/nova-notifications')
-                    .then(() => {
-                        this.notifications = []
-                        this.count = 0
-                    })
-            }
-        }
+export default {
+  data() {
+    return {
+      count: 0,
+      notifications: [],
+      isLoading: true,
     }
+  },
+  mounted() {
+    const self = this
+
+    self.loadNotifications(function (response) {
+      Echo.private(Nova.config.user_model_namespace + '.' + response.data.user_id)
+          .notification(self.notificationReceived)
+    })
+
+    Nova.$on('notification-read', function (e) {
+      self.notifications[e.notification.id] = e.notification
+      self.count--;
+    })
+  },
+  methods: {
+    loadNotifications: function (callback) {
+      axios
+          .get('/nova-vendor/nova-notifications/unread')
+          .then(response => {
+            this.isLoading = false
+            this.count = response.data.count
+            this.notifications = response.data.notifications
+
+            if (callback) {
+              callback(response)
+            }
+          })
+    },
+    notificationReceived: function (notification) {
+      const self = this
+
+      self.loadNotifications()
+
+      let level = 'info'
+      const levels = ['success', 'info', 'error']
+
+      if (levels.indexOf(notification.level) !== -1) {
+        level = notification.level
+      }
+
+      const markAsRead = {
+        text: self.__('Mark as Read'),
+        onClick: (e, toast) => {
+          self.$refs['notification-' + notification.id][0].markAsRead()
+          toast.goAway(0);
+        }
+      }
+
+      const cancel = {
+        text: self.__('Cancel'),
+        onClick: (e, toast) => {
+          toast.goAway(0);
+        }
+      }
+
+      let actions = [];
+
+      if (!('actions' in notification || notification.actions.length > 0)) {
+
+        if (notification.show_mark_as_read) {
+          actions.push(markAsRead)
+        }
+
+        if (notification.show_cancel) {
+          actions.push(cancel)
+        }
+      } else {
+        notification.actions.forEach(action => {
+          actions.push({
+                text: self.__(action.label),
+                onClick: (e, toast) => {
+                  self.$refs['notification-' + notification.id][0].handleAction(action)
+                  toast.goAway(0);
+                }
+              }
+          )
+        })
+      }
+
+      self.$toasted.show(notification.title, {
+        type: level,
+        keepOnHover: true,
+        icon: notification.icon || null,
+        iconPack: 'custom-class',
+        action: actions,
+      })
+    },
+    markAllAsRead: function () {
+      axios
+          .patch('/nova-vendor/nova-notifications')
+          .then(() => {
+            this.notifications = []
+            this.count = 0
+          })
+    }
+  }
+}
 </script>
 
 <style scoped>
-    #max-height {
-        max-height: 30rem;
-    }
+#max-height {
+  max-height: 30rem;
+}
 
-    .bg-light-gray {
-        background-color: #EEEEEE;
-    }
+.bg-light-gray {
+  background-color: #EEEEEE;
+}
 
-    .bg-light-gray:hover {
-        background-color: #fefefe;
-    }
+.bg-light-gray:hover {
+  background-color: #fefefe;
+}
 </style>
